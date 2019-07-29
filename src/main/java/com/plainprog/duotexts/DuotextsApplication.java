7cb54aeb7e70c51ad983aa3b26ec440f33e7f1ec
EIK <petrodarchyn@gmail.com>
2019-07-29 19:59:10 +0300

    First commit

---
+++ src/main/java/com/plainprog/duotexts/DuotextsApplication.java
@@ -0,0 +1,13 @@
+package com.plainprog.duotexts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuotextsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuotextsApplication.class, args);
+	}
+
+}
